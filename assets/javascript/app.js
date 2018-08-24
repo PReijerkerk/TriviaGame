83ce3ecde5e4e7460a23415a927edb182535d1c7
P Reijerkerk <preijerkerk@gmail.com>
2018-08-23 21:41:17 -0400

    created functions for the start/restart buttons; created functions to run the timer as well as decrement time and push the correct answer to the answer div box

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,48 @@
 $(document).ready(function(){
 //Declare Global variables
 var timerNumber = 30;
+var intervalID;
 var numCorrect = 0;
 var numIncorrect = 0;
-var numAnswered = 0;
+var numUnanswered = 0;
 var answers= [];
 var currentQuestion = 0;
+var running = false;
 //Objects to hold questions/ answer choices
 var trivia = [
     {
         question: "In what month is Harry Potter's birthday?",
-        correctAnswer: 3,
+        correctAnswer: "July",
         multChoice: ["November", "March", "June", "July"]
     },
 
     {
         question: "Who was Harry's third Defense Against the Dark Arts Teacher?",
-        correctAnswer: 3,
+        correctAnswer: "Professor Quirrel",
         multChoice: ["Professor Lockhart", "Professor Snape", "Professor Lupin", "Professor Quirrel"]
     },
 
     {
         question: "Which language, like Voldemort, is Harry able to speak?",
-        correctAnswer: 1,
+        correctAnswer: "Parseltongue",
         multChoice: ["Mermish", "Parseltongue", "Elfish", "Troll"]
     },
 
     {
         question: "Which Quidditch team does Ron support?",
-        correctAnswer: 0,
+        correctAnswer: "Chudley cannons",
         multChoice: ["Chudley Cannons", "Puddlemore United", "Wimbourne Wasps", "Tutshill Tornados"]
     },
 
     {
         question: "How many brothers and sisters does Ron have?",
-        correctAnswer: 2,
+        correctAnswer: "six",
         multChoice: ["Two", "Ten", "Six", "Eight"]
     },
 
     {
         question: "What is the name of Hagrid's giant brother?",
-        correctAnswer: 2,
+        correctAnswer: "Grawp",
         multChoice: ["Gawp", "Karkus", "Grawp", "Golgomath"]
     }
 ];
@@ -49,19 +51,70 @@ var trivia = [
 var question = function() {
     if (currentQuestion <= 6) {
         $("#question").html(trivia[currentQuestion].question);
+            //Displays the answers to the answer IDs
             answers = trivia[currentQuestion].multChoice;
-                for(var i = 0; i < answers.length; i++) {
-                    $(".answers + i").html("answers[i]");
-                }
-    }
+                $("#answer0").html(answers[0]);
+                $("#answer1").html(answers[1]);
+                $("#answer2").html(answers[2]);
+                $("#answer3").html(answers[3]);
+            }
 };
 
 question();
-//Display the question to the questionDiv
-//Function to grab the answers to the question
-//Display the answers to the answersDiv
-//Function to display the 60 second time limit per question
-//Function to start the timer when the question loads
+
+//Start Button
+$(".start").on("click", function() {
+    $(".start").hide();
+    $(".restart").show();
+    question();
+    runTimer();
+})
+
+//Timer begin
+function runTimer() {
+    if (!running) {
+        intervalID = setInterval(decrement, 1000);
+        running = true;
+    }
+}
+
+//Timer functionality
+function decrement() {
+    $("#timerHolder").html("<h3>Time Remaining: " + timerNumber + "</h3>");
+    timerNumber--;
+
+//stops timer upon reaching 0 seconds remaining
+    if(timerNumber === -1) {
+        numUnanswered++;
+        stop();
+        var correctA = trivia[currentQuestion].correctAnswer;
+        $("#answerDiv").html("Time has run out! The correct answer is: " +correctA);
+    }
+}
+
+//Stop timer function
+function stop() {
+    running = false;
+    clearInterval(intervalID);
+}
+
+//Restart button
+$(".restart").on("click", function() {
+    $("#question").empty();
+    $("#answer0").empty();
+    $("#answer1").empty();
+    $("#answer2").empty();
+    $("#answer3").empty();
+    stop();
+    timerNumber= 30;
+    runTimer();
+    question(); 
+})
+
+
+
+
+
 
 
 
